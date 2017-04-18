6871abf1ddc444e7873c504472919e1a3c885db9
Heather Nordquist <heathernordquist@gmail.com>
2017-04-18 14:14:43 -0600

    Add files via upload

---
+++ MIC_Installer/Register_MIC_Components.bat
@@ -0,0 +1,19 @@
+regsvr32 -s CoAPCUPSISO.dll
+regsvr32 -s CoAquilaUPSISO.dll
+regsvr32 -s CoColorButton.dll
+regsvr32 -s CoDirectSerialCSO.dll
+regsvr32 -s CoDSPECISO.dll
+regsvr32 -s CoEventISO.dll
+regsvr32 -s CoGrandISO.dll
+regsvr32 -s CoEOSSISO.dll
+regsvr32 -s CoEOSSCSO.dll
+regsvr32 -s CoPSRISO.dll
+regsvr32 -s CoHHMRISO.dll
+regsvr32 -s CoILONCSO.dll
+regsvr32 -s CoIPXCSO.dll
+regsvr32 -s CoISRISO.dll
+regsvr32 -s CoJSRISO.dll
+regsvr32 -s CoMCAISO.dll
+regsvr32 -s CoWatchISO.dll
+
+
